5d4dc32335eaf88f796934b0b3b4b89be9cea490
Ali <aliozturk@outlook.dk>
2021-08-30 10:40:54 +0200

    Initial commit

---
+++ src/main/java/com/example/demo/MessageDTO.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+public class MessageDTO {
+
+    private String message;
+
+    public String getMessage() {
+
+        return message;
+
+    }
+
+    public void setMessage(String message) {
+
+        this.message = message;
+
+    }
+
+}
